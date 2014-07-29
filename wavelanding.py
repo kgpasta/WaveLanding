f00a8c6117af1dbf85a3c386ae39c1c7ad95f3fe
kgpasta <kaustubh.garimella@gmail.com>
2014-07-28 19:28:47 -0500

    Added security

--- wavelanding.py
+++ wavelanding.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    return render_template('security.html')
 	
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	print(request.method)
+	if request.form['username'] == 'wave' and request.form['password'] == 'ridethewave':
+		return render_template('index.html')
+	else:
+		return render_template('security.html')
+
 if __name__ == '__main__':
     app.run()
